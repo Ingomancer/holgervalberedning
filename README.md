3ba449742ceb56f089136aec7f0b4185243eb530
Ingo Junefjord <lord.ingo@gmail.com>
2021-11-25 21:05:05 +0100

    Förbättringar 2021

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Om man tar in nomineringar via google forms (Fråga henrik exakt hur han gjorde!) kan man exportera som csv, som det här scriptet sedan gosar med
+Om man tar in nomineringar via google forms, på formatet:
+    Vilken ansvarspost?
+    Vem?
+    Varför?
+och sedan fyller i folks mailadresser och exporterar som csv, kan det här scriptet generera alla nomineringsmail:
+    python3 nominering.py
+vilket skapar katalogen "mail" med ett mail per person. Kontrollera dessa så det ser rätt ut!
+Sedan kan man skicka alla mail:
+    python3 nomiering.py mail
+vilket automatiskt skickar alla mail, om man angett en giltig mail/lösenords-kombo. 
+För gmail behöver man använda ett app password istället för sitt vanliga, se: https://support.google.com/accounts/answer/185833?hl=en
--- nominering.py
+++ nominering.py
@@ -1,13 +1,19 @@
 import csv
 import os
+import sys
+import smtplib
+from email.message import EmailMessage
 
 valberedningen = "Test1 och Test2"
 deadline = "en dag (ett datum)"
 svarsmail = "mail@mail.mail"
 tjatdag = "nåndag"
-nomineringsfil = "ExempelNomineringar.csv "
+nomineringsfil = "ExempelNomineringar.csv"
+mail_dir = "mail"
+gmail_user = "mail@mail.mail"
+gmail_password = 'NOPE'
 
-mail_template = """
+mail_template = """\
 Hej​! ​
 
 Nu har vi sammanställt nomineringarna och du har blivit nominerad till följande poster:
@@ -16,7 +22,7 @@ Nu har vi sammanställt nomineringarna och du har blivit nominerad till följand
 
 Vi vill att du tar och funderar på hur du ställer dig till nomineringarna. Senast​ {deadline} behöver vi veta ditt svar per nominering (ja, nej, kanske). Väljer du att skriva kanske så vill vi ha en förklaring tex: Behöver få reda på om jag har fått det där extrajobbet jag sökte först.
 
-Du svarar genom att maila tillbaka till {svarsmail} när du väl har tagit ställning och har sagt ja till en eller flera nomineringar
+Du svarar genom att maila till mig eller {svarsmail} när du väl har tagit ställning och har sagt ja till en eller flera nomineringar
 
 Är du sugen på en eller flera poster så vill vi ha in en motivering till varför du skulle passa bra på just den posten. Detta för att vi ska ha lite mer på fötterna när vi ska arbeta med att ta fram vårt förslag till årsmötet.
 
@@ -28,22 +34,45 @@ Vad händer sen
     När vi har fått fram vårt förslag så kommer vi att ringa er som vi vill föreslå samt er som ​sagt ja/kanske som ​vi inte kommer föreslå så att ni har möjlighet att motkandidera om ni vill.
     På årsmötet kommer hela föreningen att bestämma om ni blir valda eller ej och vem som helst kan motkandidera till de olika posterna.
 
-Här är en länk till Holgerspexets policydokument, där de flesta ansvarsposterna är beskrivna: https://www.lysator.liu.se/~hx/holgerpolicy.pdf
+Här är en länk till Holgerspexets policydokument, där de flesta ansvarsposterna är beskrivna: https://insidan.holgerspexet.se/arkivet/#/Styrdokument/
 
-Har du någon fråga eller fundering så är det bara att ringa eller maila oss.
+Har du någon fråga eller fundering så är det bara att ringa eller maila oss, kontaktuppgifter finns på insidan.
 
 Enorma kramar från {valberedningen}"""
 
-with open (nomineringsfil, encoding='UTF-8') as csvfile:
-    reader = csv.reader(csvfile)
-    nomineringar = {}
-    for line in reader:
-        try:
-            nomineringar[line[2]].add(line[1])
-        except KeyError:
-            nomineringar[line[2]] = set()
-            nomineringar[line[2]].add(line[1])
-    os.makedirs('mail', exist_ok=True)
-    for namn, poster in nomineringar.items():
-        with open('mail/'+namn+'.txt', 'w', encoding='UTF-8') as output:
-            output.write(mail_template.format(poster="\n".join(poster), deadline=deadline, svarsmail=svarsmail, tjatdag=tjatdag, valberedningen=valberedningen))
+def generate_emails():
+    with open (nomineringsfil, encoding='UTF-8') as csvfile:
+        reader = csv.reader(csvfile)
+        nomineringar = {}
+        for line in reader:
+            try:
+                nomineringar[(line[2], line[4])].add(line[1])
+            except KeyError:
+                nomineringar[(line[2], line[4])] = set()
+                nomineringar[(line[2], line[4])].add(line[1])
+        os.makedirs('mail', exist_ok=True)
+        for (namn, email), poster in nomineringar.items():
+            with open(f'{mail_dir}/{email}', 'w', encoding='UTF-8') as output:
+                output.write(mail_template.format(poster="\n".join(poster), deadline=deadline, svarsmail=svarsmail, tjatdag=tjatdag, valberedningen=valberedningen))
+
+def send_emails():
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(gmail_user, gmail_password)
+    for filename in os.listdir(mail_dir):
+        mail = filename
+        with open(f"{mail_dir}/{filename}", 'r', encoding="UTF-8") as f:
+            content = f.read()
+        msg = EmailMessage()
+        msg.set_content(content)
+        msg['Subject'] = "Du har blivit nominerad!"
+        msg['From'] = gmail_user
+        msg['To'] = mail
+        server.send_message(msg)
+    server.close
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2 and sys.argv[1] == 'mail':
+        send_emails()
+    else:
+        generate_emails()
